87fea675d3bdd914952956e716b86772c420844a
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-06-01 19:33:30 +0500

    Add unit tests for factorial, reverse, and fizzbuzz methods

---
+++ spec/solver_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../solver.rb'
+
+describe "Solver" do
+  
+    let(:solver) { Solver.new() }
+
+    describe 'Factorial' do
+      it 'returns the factorial of a number' do
+        expect(solver.factorial(5)).to eq(120)
+      end
+    end
+  
+    describe 'Reverse' do
+      it 'returns the reverse of given string' do
+        expect(solver.reverse("hello")).to eq("olleh")
+      end
+    end
+  
+    describe 'fizzbuzz' do
+      it 'Returns fizz if number is divisble by 3' do
+        expect(solver.fizzbuzz(15)).to eq('fizz')
+      end
+      it 'Returns buzz if number is divisble by 5' do
+        expect(solver.fizzbuzz(25)).to eq('buzz')
+      end
+      it 'Returns fizzbuzz if number is divisble by both 5 and 3' do
+        expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      end
+    end
+  end

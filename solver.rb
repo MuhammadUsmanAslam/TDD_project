bb06cfe01cd324039a44b36c2653def60cef64a5
ikoote1 <ikootepreim@gmail.com>
2023-06-01 18:13:14 +0300

    reversd method

--- solver.rb
+++ solver.rb
@@ -1,4 +1,10 @@
 class Solver
+
+    def reverse(word)
+        reversed = word.reverse
+        return reversed
+    end
+
     def fizzbuzz(n)
         if n%3 == 0 && n%5 == 0
             return 'fizzbuzz'
